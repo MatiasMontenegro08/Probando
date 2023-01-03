1a0a053746b742a046fe36aaa5f2344852090a53
matias nicolas montenegro <matiasmontenegro08@gmail.com>
2023-01-03 15:23:41 -0300

    Mensajeria

---
+++ Mensajeria/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Mensajeria
+{
+    partial class Mensajeria
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mensajeria));
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtUrgente = new System.Windows.Forms.RadioButton();
+            this.rbtPaqNormal = new System.Windows.Forms.RadioButton();
+            this.rbtNormal = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkFax = new System.Windows.Forms.CheckBox();
+            this.chkTel = new System.Windows.Forms.CheckBox();
+            this.chkEmail = new System.Windows.Forms.CheckBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlBarra = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlBarra.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMinimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(710, 3);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(36, 35);
+            this.btnMinimizar.TabIndex = 2;
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.Location = new System.Drawing.Point(752, 3);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(36, 35);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(11, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COMO DESEA RECIBIR EL PEDIDO?";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.rbtUrgente);
+            this.groupBox1.Controls.Add(this.rbtPaqNormal);
+            this.groupBox1.Controls.Add(this.rbtNormal);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(15, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 123);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbtUrgente
+            // 
+            this.rbtUrgente.AutoSize = true;
+            this.rbtUrgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtUrgente.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbtUrgente.Location = new System.Drawing.Point(23, 90);
+            this.rbtUrgente.Name = "rbtUrgente";
+            this.rbtUrgente.Size = new System.Drawing.Size(224, 22);
+            this.rbtUrgente.TabIndex = 2;
+            this.rbtUrgente.TabStop = true;
+            this.rbtUrgente.Text = "Por paquetería Urgente ($300)";
+            this.rbtUrgente.UseVisualStyleBackColor = true;
+            // 
+            // rbtPaqNormal
+            // 
+            this.rbtPaqNormal.AutoSize = true;
+            this.rbtPaqNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtPaqNormal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbtPaqNormal.Location = new System.Drawing.Point(23, 55);
+            this.rbtPaqNormal.Name = "rbtPaqNormal";
+            this.rbtPaqNormal.Size = new System.Drawing.Size(218, 22);
+            this.rbtPaqNormal.TabIndex = 1;
+            this.rbtPaqNormal.TabStop = true;
+            this.rbtPaqNormal.Text = "Por paquetería normal ($200)";
+            this.rbtPaqNormal.UseVisualStyleBackColor = true;
+            // 
+            // rbtNormal
+            // 
+            this.rbtNormal.AutoSize = true;
+            this.rbtNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtNormal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbtNormal.Location = new System.Drawing.Point(23, 20);
+            this.rbtNormal.Name = "rbtNormal";
+            this.rbtNormal.Size = new System.Drawing.Size(194, 22);
+            this.rbtNormal.TabIndex = 0;
+            this.rbtNormal.TabStop = true;
+            this.rbtNormal.Text = "Por correo normal ($150)";
+            this.rbtNormal.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(14, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "COMO DESEA SER NOTIFICADO DEL ENVÍO?";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox2.Controls.Add(this.chkFax);
+            this.groupBox2.Controls.Add(this.chkTel);
+            this.groupBox2.Controls.Add(this.chkEmail);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.Location = new System.Drawing.Point(15, 226);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 122);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // chkFax
+            // 
+            this.chkFax.AutoSize = true;
+            this.chkFax.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFax.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.chkFax.Location = new System.Drawing.Point(22, 89);
+            this.chkFax.Name = "chkFax";
+            this.chkFax.Size = new System.Drawing.Size(130, 22);
+            this.chkFax.TabIndex = 2;
+            this.chkFax.Text = "Por Fax ($50)";
+            this.chkFax.UseVisualStyleBackColor = true;
+            // 
+            // chkTel
+            // 
+            this.chkTel.AutoSize = true;
+            this.chkTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkTel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.chkTel.Location = new System.Drawing.Point(22, 55);
+            this.chkTel.Name = "chkTel";
+            this.chkTel.Size = new System.Drawing.Size(169, 22);
+            this.chkTel.TabIndex = 1;
+            this.chkTel.Text = "Por Teléfono ($30)";
+            this.chkTel.UseVisualStyleBackColor = true;
+            // 
+            // chkEmail
+            // 
+            this.chkEmail.AutoSize = true;
+            this.chkEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkEmail.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.chkEmail.Location = new System.Drawing.Point(22, 21);
+            this.chkEmail.Name = "chkEmail";
+            this.chkEmail.Size = new System.Drawing.Size(145, 22);
+            this.chkEmail.TabIndex = 0;
+            this.chkEmail.Text = "Por Email ($10)";
+            this.chkEmail.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCalcular.Image = ((System.Drawing.Image)(resources.GetObject("btnCalcular.Image")));
+            this.btnCalcular.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCalcular.Location = new System.Drawing.Point(99, 354);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(157, 43);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(334, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 374);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlBarra
+            // 
+            this.pnlBarra.Controls.Add(this.btnMinimizar);
+            this.pnlBarra.Controls.Add(this.btnSalir);
+            this.pnlBarra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBarra.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarra.Name = "pnlBarra";
+            this.pnlBarra.Size = new System.Drawing.Size(800, 44);
+            this.pnlBarra.TabIndex = 8;
+            this.pnlBarra.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlBarra_MouseDown);
+            // 
+            // Mensajeria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 406);
+            this.Controls.Add(this.pnlBarra);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Mensajeria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mensajeria";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlBarra.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtUrgente;
+        private System.Windows.Forms.RadioButton rbtPaqNormal;
+        private System.Windows.Forms.RadioButton rbtNormal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkFax;
+        private System.Windows.Forms.CheckBox chkTel;
+        private System.Windows.Forms.CheckBox chkEmail;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlBarra;
+    }
+}
+
---
+++ Mensajeria/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mensajeria
+{
+    public partial class Mensajeria : Form
+    {
+        double resultado, recargo;
+        string  aviso, extra;
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        public Mensajeria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (rbtNormal.Checked)
+            {
+                Calcular();
+                resultado = 150 + recargo;
+                aviso = "ENVÍO NORMAL";
+                Mostrar();
+            }
+            if (rbtPaqNormal.Checked)
+            {
+                Calcular();
+                resultado = 200 + recargo;
+                aviso = "PAQUETERÍA NORMAL";
+                Mostrar();
+            }
+            if (rbtUrgente.Checked)
+            {
+                Calcular();
+                resultado = 300 + recargo;
+                aviso = "PARQUETERÍA URGENTE";
+                Mostrar();
+            }
+        }
+
+        private void pnlBarra_Paint(object sender, PaintEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void pnlBarra_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        void Calcular()
+        {
+            recargo = 0;
+            extra = "";
+            if (chkEmail.Checked)
+            {
+                recargo += 10;
+                extra += "Email.-";
+            }
+            if (chkTel.Checked)
+            {
+                recargo += 30;
+                extra += "Teléfono.-";
+            }
+            if (chkFax.Checked)
+            {
+                recargo += 50;
+                extra += "Fax.";
+            }
+        }
+        void Mostrar()
+        {
+
+            MessageBox.Show("Usted seleccionó el envío por " + aviso + " y notificar por " + extra + "\nEl total del envío es de: $" + resultado.ToString("N2"));
+        }
+    }
+}
